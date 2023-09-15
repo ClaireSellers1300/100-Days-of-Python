c390717e72199188c25ecfe77c5396fb5bde4056
Claire Sellers <139182297+ClaireSellers1300@users.noreply.github.com>
2023-09-15 16:06:15 +0100

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,1 +1,32 @@
+#myName = input("What's your name? ")
+#myLunch = input("What are you having for lunch? ")
+#print(myName, "is going to be chowing down on", myLunch, "very soon!")
 
+#number = input("Give me a number: ")
+#group = input("Give me a collective noun for a group of things: ")
+#thing = input("Give me the name of a weird or wacky thing: ")
+#print("No I don't think that", number, "is a", group, "of", thing,""". That's just odd.""")
+
+#yourName = input("Name: ")
+#whatYear = input("What year is it?: ")
+#print(yourName, "thinks it is", whatYear)
+
+#print("=== Your Song Generator ===")
+#print("""You'll be asked a bunch of questions
+#then we'll make you up an amazing
+#song, totally copyright free 😭""")
+#print()
+#person = input("Name a person famous for something good: ")
+#thing = input("Name a thing they did: ")
+#place = input("Name a place you like: ")
+#rhyme = input("Give me a verb that rhymes with your person's name: ")
+#print()
+#print("There was a person called", person,"Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
+
+print("Welcome to the wacky recipe generator. Please input the following:  ")
+food= input("A favourite food:  ")
+plant= input("A funky plant:  ")
+item= input("A household item:  ")
+cooking= input("method of cooking:  ")
+burnt= input("A fancy cooking term for burnt food:  ")
+print("Please enjoy tonight's dish of",cooking, food, "with", burnt, plant, "on a bed of", item,"!")
